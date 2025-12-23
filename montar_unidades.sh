822e0294d9d1fc8f65df2d927d9fe5839602408c
sukigsx <sukigsx.mbsistemas@gmail.com>
2025-12-23 19:17:41 +0100

    Update montar_unidades.sh

--- montar_unidades.sh
+++ montar_unidades.sh
@@ -732,7 +732,7 @@ echo -e ""
         conexion="no" #sabemos si tenemos conexion a internet o no
     fi
 
-for paquete in net-tools figlet smbclient git nano cifs-utils nmap diff #ponemos el fostware a instalar separado por espacios
+for paquete in figlet smbclient git nano cifs-utils nmap diff #ponemos el fostware a instalar separado por espacios
 do
     which $paquete 2>/dev/null 1>/dev/null 0>/dev/null #comprueba si esta el programa llamado programa
     sino=$? #recojemos el 0 o 1 del resultado de which
